1a1f91c17361458cf07b0d51915fbac14f449318
JeffRisberg <jrisberg@aol.com>
2017-12-24 19:26:48 -0800

    added query endpoint

--- src/main/java/com/company/endpoints/QueryEndpoint.java
+++ src/main/java/com/company/endpoints/QueryEndpoint.java
@@ -1,11 +1,25 @@
 package com.company.endpoints;
 
+import org.elasticsearch.client.Client;
+import org.elasticsearch.common.settings.Settings;
+import org.elasticsearch.common.transport.TransportAddress;
+import org.elasticsearch.index.query.QueryBuilder;
+import org.elasticsearch.index.query.QueryBuilders;
+import org.elasticsearch.search.SearchHit;
+import org.elasticsearch.transport.client.PreBuiltTransportClient;
+
 import javax.inject.Inject;
 import javax.inject.Singleton;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.GenericEntity;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import java.net.InetSocketAddress;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @author Jeff Risberg
@@ -15,14 +29,37 @@ import javax.ws.rs.core.Response;
 @Path("query")
 public class QueryEndpoint {
 
+    String clusterName = "elasticsearch";
+    String indexName = "product";
+
+    protected Client client;
+
     @Inject
     public QueryEndpoint() {
+
+        Settings settings = Settings.builder()
+                .put("cluster.name", clusterName).build();
+
+        this.client = new PreBuiltTransportClient(settings)
+                .addTransportAddress(new TransportAddress(new InetSocketAddress("127.0.0.1", 9300)));
     }
 
     @GET
     @Path("{query}")
-    public Response handle(@PathParam("query") String query) {
-        Object results = query;
-        return Response.status(Response.Status.OK).entity(results).build();
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response handle(@PathParam("query") String queryStr) {
+        QueryBuilder query = QueryBuilders.matchAllQuery();
+        System.out.println("getMatchAllQueryCount query => " + query.toString());
+
+        SearchHit[] hits = client.prepareSearch(indexName).setQuery(query).execute().actionGet().getHits().getHits();
+
+        List<String> list = new ArrayList<String>();
+        for (SearchHit hit : hits) {
+            list.add(hit.getSourceAsString());
+        }
+
+        GenericEntity<List<String>> entity = new GenericEntity<List<String>>(list) {};
+
+        return Response.status(Response.Status.OK).entity(entity).build();
     }
 }
