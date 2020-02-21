acdb38ccf8af635f79fca9b2c90efcee4008418b
JeffRisberg <jrisberg@aol.com>
2020-02-21 11:44:08 -0800

    added SearchQuery and searchResult classes, which are used by SearchService API.

--- src/main/java/com/company/service/SearchService.java
+++ src/main/java/com/company/service/SearchService.java
@@ -2,18 +2,36 @@ package com.company.service;
 
 import com.company.common.SearchQuery;
 import com.company.common.SearchResultRecord;
+import org.elasticsearch.client.Client;
+import org.elasticsearch.index.query.QueryBuilder;
+import org.elasticsearch.search.SearchHit;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.function.Consumer;
+
+import static org.elasticsearch.index.query.QueryBuilders.matchAllQuery;
 
 public class SearchService {
 
-    /**
-     *
-     * @param query query description
-     * @param consumer object to consume search result
-     */
-    void searchFulltext(SearchQuery query, Consumer<SearchResultRecord> consumer) {
+  Client client;
 
+  public SearchService(Client client) {
+    this.client = client;
+  }
+
+  /**
+   * @param query    query description
+   * @param consumer object to consume search result
+   */
+  void searchFulltext(SearchQuery query, Consumer<SearchResultRecord> consumer) {
+    QueryBuilder thisQuery = matchAllQuery();
+    System.out.println("getMatchAllQueryCount query =>" + query.toString());
+    SearchHit[] hits = client.prepareSearch("products").setQuery(thisQuery).execute().actionGet().getHits().getHits();
+
+    for (SearchHit hit : hits) {
+      // create SearchResultRecord
+      // send to consumer
     }
-
+  }
 }
